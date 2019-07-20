5325953895253433cfcd01986ba2cbb55097a656
jsomers1 <52543952+jsomers1@users.noreply.github.com>
2019-07-19 19:51:32 -0500

    added copying command

--- macgyver_model.sh
+++ macgyver_model.sh
@@ -17,6 +17,7 @@ docker cp Desktop/test1/ _containerIDnum_:/
 # Check directories in container are correct
 docker exec -it _containerIDnum_ bash
 cd /
+ls
 
 # Train model
 python tensorflow/tensorflow/examples/image_retraining/retrain.py  --bottleneck_dir=/bottlenecks  --model_dir=/inception  --output_labels=/retrained_labels1.txt  --output_graph=/retrained_graph.pb  --image_dir=/training_images1
@@ -25,3 +26,6 @@ python tensorflow/tensorflow/examples/image_retraining/retrain.py  --bottleneck_
 python tensorflow/tensorflow/examples/image_retraining/label_image.py  --graph=/retrained_graph.pb  --labels=/retrained_labels.txt  --image=/test1/1050.jpg
 python tensorflow/tensorflow/examples/image_retraining/label_image.py  --graph=/retrained_graph.pb  --labels=/retrained_labels.txt  --image=/test1/827.jpg
 python tensorflow/tensorflow/examples/label_image/label_image.py \
+
+# Use for copying to / from Docker and host
+# docker cp _containerIDnum_:/test1/1001.jpg /Users/js/Desktop
